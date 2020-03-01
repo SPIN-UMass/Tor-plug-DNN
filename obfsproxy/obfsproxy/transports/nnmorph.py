40feefb01349b61990788c8f23af9f8000b03156
milad <milad@cs.umass.edu>
2020-03-01 18:40:00 -0500

    saves

--- obfsproxy/obfsproxy/transports/nnmorph.py
+++ obfsproxy/obfsproxy/transports/nnmorph.py
@@ -7,27 +7,14 @@ The obfs3 module implements the obfs3 protocol.
 
 import random
 
-import obfsproxy.common.aes as aes
-import obfsproxy.transports.base as base
-import obfsproxy.transports.obfs3_dh as obfs3_dh
-import obfsproxy.common.log as logging
-import obfsproxy.common.hmac_sha256 as hmac_sha256
-import obfsproxy.common.rand as rand
-
 from twisted.internet import threads
+
+from  collections import deque
 
 log = logging.get_obfslogger()
 
-MAX_PADDING = 8194
-
-PUBKEY_LEN = 192
-KEYLEN = 16  # is the length of the key used by E(K,s) -- that is, 16.
-HASHLEN = 32 # length of output of sha256
-
-ST_WAIT_FOR_KEY = 0 # Waiting for public key from the other party
-ST_WAIT_FOR_HANDSHAKE = 1 # Waiting for the DH handshake
-ST_SEARCHING_MAGIC = 2 # Waiting for magic strings from the other party
-ST_OPEN = 3 # obfs3 handshake is complete. Sending application data.
+HISTORY_LEN = 10
+import time
 
 class NNTransport(base.BaseTransport):
     
@@ -35,6 +22,8 @@ class NNTransport(base.BaseTransport):
     def __init__(self):
         """Initialize the obfs3 pluggable transport."""
         super(NNTransport, self).__init__()
+        self.sent_packets = deque(maxlen = HISTORY_LEN)
+        self.rcvd_packets = deque(maxlen = HISTORY_LEN)
 
     def circuitConnected(self):
         
@@ -49,7 +38,8 @@ class NNTransport(base.BaseTransport):
         """
         message = data.read()
         log.debug("nn receivedUpstream: Transmitting %d bytes.", len(message))
-
+        
+        self.sent_packets.append((time.time(),len(message)))
         # Proxy encrypted message.
         self.circuit.downstream.write(message)
 
@@ -60,6 +50,11 @@ class NNTransport(base.BaseTransport):
         """
         log.debug("nn receivedDownstream: Processing %d bytes of application data." %
                     len(data))
+
+        self.rcvd_packets.append((time.time(),len(data)))
+        
+        log.debug(self.rcvd_packets)
+        
         self.circuit.upstream.write(data.read())
 
     
